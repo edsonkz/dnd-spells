5ba799841d462293149c0c72d5f3ce22dc320907
edsonkz <edsonbisneto12@gmail.com>
2022-02-12 09:24:08 -0300

    Adicionada descrição aos spells.

--- src/App.js
+++ src/App.js
@@ -6,25 +6,33 @@ import { useEffect, useState } from "react";
 function App() {
 	const [spells, setSpells] = useState([]);
 
-	useEffect(() => {
-		api.get("/spells")
-			.then((response) => {
-				console.log(response.data.results);
-				return setSpells(response.data.results);
-			})
-			.catch((err) => {
-				console.error("Requisição falhou! " + err);
-			});
+	useEffect(async () => {
+		try{
+			const response = await api.get("spells");
+			var {results} = response.data;
+
+			for (var i = 0; i < results.length; i++){
+				results[i].description = await (await api.get(results[i].url.substring(5))).data.desc;
+			}
+
+			return setSpells(results);
+
+		} catch (err){
+			console.error("Requisição falhou! " + err);
+		}
 	}, []);
 
 	return (
 		<div className="App">
 			{spells.length > 0 ? (
 				spells.map((spell) => {
-					return <li key={spell.name}>{spell.name}</li>;
+					return (<div key={spell.name}>
+						<h1>{spell.name}</h1>
+						<a>{spell.description}</a>
+					</div>);
 				})
 			) : (
-				<a>Ronaldo</a>
+				<a>Loading...</a>
 			)}
 		</div>
 	);
