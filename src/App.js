ac636259ffdd26bca22c0debcd682dbf1c6d0d27
edsonkz <edsonbisneto12@gmail.com>
2022-03-05 10:06:25 -0300

    A lista de spells agora é atualizada cada vez que é chegado no fim da página.

--- src/App.js
+++ src/App.js
@@ -2,25 +2,59 @@ import "./App.css";
 import api from "./services/api";
 import SpellItem from "./Spell";
 import { useEffect, useState } from "react";
+import { useBottomScrollListener } from 'react-bottom-scroll-listener';
+ 
 
 function App() {
 	const [spells, setSpells] = useState([]);
 	const [currentSpells, setCurrentSpells] = useState([]);
-	const [page, setPage] = useState(1);
-	const [lastSpell, setLastSpell] = useState();
+	const [totalSpells, setTotalSpells] = useState(0);
+	const [numberOfSpells, setNumberOfSpells] = useState(0);
+	
+	async function loadMore() {
+		let cSpells = currentSpells;
+		let requestSpells = 0;
+		if (cSpells.length + 25 > totalSpells){
+			requestSpells = totalSpells - cSpells.length;
+		} else if (cSpells.length === totalSpells){
+			return;
+		} else {
+			requestSpells = 25;
+		}
 
+		try{
+			for (var i = numberOfSpells; i < numberOfSpells + requestSpells; i++){
+				let aux = {}
+				aux.name = spells[i].name;
+				aux.description = (await api.get(spells[i].url.substring(5))).data.desc;
+				console.log(aux);
+				cSpells[i] = aux;
+			}
+			//console.log(cSpells);
+	
+			return setCurrentSpells(cSpells), setNumberOfSpells(numberOfSpells + requestSpells);
+		} catch (err){
+			console.error("Requisição falhou! " + err);
+		}
+	}
+	useBottomScrollListener(loadMore);
+	
 	useEffect(async () => {
 		try{
 			const response = await api.get("spells");
-			var {results} = response.data;
-			var cSpells = [];
-			for (var i = 0; i < 25; i++){
-				results[i].description = (await api.get(results[i].url.substring(5))).data.desc;
-				cSpells[i] = results[i]
+			const {results} = response.data;
+			console.log(response.data.count);
+			let cSpells = [];
+			for (var i = numberOfSpells; i < numberOfSpells + 25; i++){
+				let aux = {}
+				console.log(i);
+				aux.name = results[i].name;
+				aux.description = (await api.get(results[i].url.substring(5))).data.desc;
+				cSpells[i] = aux;
 			}
+			//console.log(cSpells);
 
-
-			return setSpells(results), setCurrentSpells(cSpells), setLastSpell(cSpells[cSpells.length - 1]);
+			return setSpells(results), setCurrentSpells(cSpells), setNumberOfSpells(numberOfSpells + 25), setTotalSpells(response.data.count);
 
 		} catch (err){
 			console.error("Requisição falhou! " + err);
@@ -30,10 +64,8 @@ function App() {
 	return (
 		<div className="App">
 			{currentSpells.length > 0 ? (
-				currentSpells.map((spell) => {
-					return (<div key={spell.name}>
-						<SpellItem spell={spell}/>
-					</div>);
+				currentSpells.map((spell, index) => {
+					return <SpellItem key={index} spell={spell}/>;
 				})
 			) : (
 				<a>Loading...</a>
